817ab5fe01f47e992df9177a790cea3356ddde9b
ozansousa <94762732+ozansousa@users.noreply.github.com>
2024-12-01 13:09:26 +0000

    Create touch_ctr.py

---
+++ touch_ctr.py
@@ -0,0 +1,20 @@
+import pyautogui
+from time import sleep
+from touch_utility import read_touch_coordinates
+
+surfaceX=480
+surfaceY=320
+
+ratioX=surfaceX / 255
+ratioY=surfaceY / 230
+
+prevX = 0
+prevY=0
+while True:
+  touch_x, touch_y = read_touch_coordinates()
+  if touch_x != 255 and touch_y !=0:
+    pyautogui.moveTo(round(touch_x * ratioX),round(touch_y * ratioY),duration=0.25)
+  if 
+  
+  
+  
