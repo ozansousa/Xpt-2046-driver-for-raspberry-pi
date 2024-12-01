9e867777e67f13f834c6b6da76736a5170425395
ozansousa <94762732+ozansousa@users.noreply.github.com>
2024-12-01 13:12:21 +0000

    Update touch_ctr.py

--- touch_ctr.py
+++ touch_ctr.py
@@ -14,7 +14,10 @@ while True:
   touch_x, touch_y = read_touch_coordinates()
   if touch_x != 255 and touch_y !=0:
     pyautogui.moveTo(round(touch_x * ratioX),round(touch_y * ratioY),duration=0.25)
-  if 
-  
+  if touch_x == prevX and touch_y == prevY and touch_x != 255 and touch_y != 0:
+    pyautogui.click()
+  prevX = touch_x
+  prevY=touch_y
+  sleep(0.1)
   
   
