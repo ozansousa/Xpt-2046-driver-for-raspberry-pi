87e2183fe3e43eb603760a94160ee1cc10ecd6b0
ozansousa <94762732+ozansousa@users.noreply.github.com>
2024-12-01 13:43:51 +0000

    Update touch_ctr.py

--- touch_ctr.py
+++ touch_ctr.py
@@ -10,10 +10,20 @@ ratioY=surfaceY / 230
 
 prevX = 0
 prevY=0
+
+flipped=False
+def rev(num, h):
+  if h == True:
+    return 480 - num
+  else:
+    return 320 - num
 while True:
   touch_x, touch_y = read_touch_coordinates()
   if touch_x != 255 and touch_y !=0:
-    pyautogui.moveTo(round(touch_x * ratioX),round(touch_y * ratioY),duration=0.25)
+    if flipped==True:
+      pyautogui.moveTo(rev(round(touch_x * ratioX),True),rev(round(touch_y * ratioY),True),duration=0.25)
+    else:
+      pyautogui.moveTo(round(touch_x * ratioX),round(touch_y * ratioY),duration=0.25)
   if touch_x == prevX and touch_y == prevY and touch_x != 255 and touch_y != 0:
     pyautogui.click()
   prevX = touch_x
